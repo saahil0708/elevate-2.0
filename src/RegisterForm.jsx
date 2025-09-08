75cc0d5cdeb1362ebcd138828587c86362328cf0
Suryavanshi11444 <modi00447@gmail.com>
2025-09-08 15:15:28 +0530

    save backend

--- src/RegisterForm.jsx
+++ src/RegisterForm.jsx
@@ -19,7 +19,7 @@ export default function RegisterForm() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/register", {
+      const res = await fetch("https://backend-eta-five-43.vercel.app/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
